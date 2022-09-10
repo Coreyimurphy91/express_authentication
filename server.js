166aae675aae7a18befef6bd1ab1c720f24ecca6
Corey Murphy <coreyimurphy@gmail.com>
2022-09-10 18:42:52 -0400

    ppConfig: Create passport

--- server.js
+++ server.js
@@ -16,14 +16,14 @@ app.use(require('morgan')('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 app.use(layouts);
-app.use(flash());            // flash middleware
 app.use(session({
   secret: SECRET_SESSION,    // What we actually will be giving the user on our site as a session cookie
   resave: false,             // Save the session even if it's modified, make this false
   saveUninitialized: true    // If we have a new session, we save it, therefore making that true
 }));
+app.use(flash());            // flash middleware
 app.use((req, res, next) => {
-  console.log(res.locals);
+  console.log('res locals ->', res.locals);
   res.locals.alerts = req.flash();
   res.locals.currentUser = req.user;
   next();
